4b673999687143620b9f475974e543ba62fcb554
Daniel Pernar <pernar.daniel@gmail.com>
2024-09-06 17:08:50 -0400

    Set transparency

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,3 +1,3 @@
 -- Set the background transparent I guess?
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" });
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" });
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" });
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" });
